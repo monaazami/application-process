1c1c2aae31c96ca0fc4b7c7c9a2582c29ba752f7
OlenaKashuba <amely.jazz@gmail.com>
2018-04-02 20:43:12 +0100

    Add ApplicantDashboard component with its children

---
+++ frontend/src/DashboardStep.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import StatusMessage from './StatusMessage';
+
+const DashboardStep = ({stepNumber, details}) => {
+	let submitBlock;
+	if (stepNumber != 0) {
+		submitBlock = 	<div>
+							<input type='text' placeholder='Add url here' required/>
+							<button type='submit'> Submit step </button> 
+						</div>
+	}
+	return (
+		<section>
+			<h3> Step {stepNumber} </h3>
+			<p> {details} </p>
+			{submitBlock}
+			<StatusMessage status={(stepNumber != 0) ? '' : 'Approved'}/>
+		</section>
+	);
+};
+
+export default DashboardStep;
