1e604a5a9631c8b37da14c7b33ce013cd6471d5b
louiaR <louitiani@gmail.com>
2018-04-02 19:50:43 +0100

    edit applicant details and indentation

--- frontend/src/Components/AdminProgress/ApplicantDetails.js
+++ frontend/src/Components/AdminProgress/ApplicantDetails.js
@@ -1,19 +1,19 @@
 import React, {Fragment} from 'react';
 
 const ApplicantDetails = (props) => {
-    
+
     return(
         <Fragment>
             <div className="Applicant-details">City: London</div>                                 
             {props.show ? 
                 (<Fragment>
-                    <div className="Applicant-details">Programming experience: Some</div> 
-                    <div className="Applicant-details">Refuges? No</div>
-                    <div className="Applicant-details">Tel: 26367484848</div>
-                    <div className="Applicant-details">Email: sdfdddgh</div>
-                    <div className="Applicant-details">Country: UK</div> 
-                    <div className="Applicant-details">Address: 44 E2 LPV </div> 
-                    <div className="Applicant-details">Referrer: </div>              
+                    <div className="Applicant-details"> Programming experience: Some </div> 
+                    <div className="Applicant-details"> Refuges? No </div>
+                    <div className="Applicant-details"> Tel: 26367484848 </div>
+                    <div className="Applicant-details"> Email: alice@gmail.com </div>
+                    <div className="Applicant-details"> Country: UK</div> 
+                    <div className="Applicant-details"> Address: 44 Queen Avenue E2 8PV </div> 
+                    <div className="Applicant-details"> Referrer: </div>              
                 </Fragment>) : null}
         </Fragment>  
     );
--- frontend/src/Components/AdminProgress/Applicants.js
+++ frontend/src/Components/AdminProgress/Applicants.js
@@ -6,9 +6,9 @@ const applicant = (props) => {
     return(
         <Fragment>
             <div className="Applicant-details">
-                <p><i className="fa fa-user-circle"></i> <span>Alice </span></p>
-                {props.show?<i class="fa fa-angle-down" onClick={props.toggle}></i>                
-                : <i className="fa fa-angle-up rotate-icon" onClick={props.toggle}></i> }
+                <p> <i className="fa fa-user-circle"></i> <span> Alice </span> </p>
+                {props.show?<i class="fa fa-angle-down" onClick={props.toggle}> </i>                
+                : <i className="fa fa-angle-up rotate-icon" onClick={props.toggle}> </i> }
             </div>
             <ApplicantDetails show={props.show}/>
         </Fragment>
