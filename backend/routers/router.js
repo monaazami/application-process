40e8588ada95cd7fdfdda103a0a41d61a9cbd39a
OlenaKashuba <amely.jazz@gmail.com>
2018-04-08 13:48:07 +0100

    Adjust 'api/applicants' route response according to a database table structure

--- backend/routers/router.js
+++ backend/routers/router.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
 
 router.get('/applicants', (req,res) => {
-	res.status(200).json({name: 'John Smith'});
+	res.status(200).json({
+		id: '3432',
+		name: 'John Smith',
+		email: 'johnsmith@gmail.com',
+		city: 'London',
+		tel: '98483247282',
+		status: 'Asylum seeker',
+		country: 'Eritrea',
+		experience: 'very little',
+		itAcess: true,
+		hearAbout: 'website'
+	});
 })
 
 module.exports=router; 
