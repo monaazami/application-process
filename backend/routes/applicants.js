f393c41d08700f61fb433760a97cc88bf66076c3
OlenaKashuba <amely.jazz@gmail.com>
2018-04-08 14:19:18 +0100

    Separate routes in folder and server.js

---
+++ backend/routes/applicants.js
@@ -0,0 +1,59 @@
+const router = require('express').Router();
+
+router.get('/', (req,res) => {
+	res.json([{
+		id: '3432',
+		name: 'John Smith',
+		email: 'johnsmith@gmail.com',
+		city: 'London',
+		tel: '98483247282',
+		status: 'Asylum seeker',
+		country: 'Eritrea',
+		experience: 'very little',
+		itAcess: true,
+		hearAbout: 'website'
+	},
+	{
+		id: '3433',
+		name: 'John Smith',
+		email: 'johnsmith@gmail.com',
+		city: 'London',
+		tel: '98483247282',
+		status: 'Asylum seeker',
+		country: 'Eritrea',
+		experience: 'very little',
+		itAcess: true,
+		hearAbout: 'website'
+	},
+	{
+		id: '3434',
+		name: 'John Smith',
+		email: 'johnsmith@gmail.com',
+		city: 'London',
+		tel: '98483247282',
+		status: 'Asylum seeker',
+		country: 'Eritrea',
+		experience: 'very little',
+		itAcess: true,
+		hearAbout: 'website'
+	}]);
+});
+
+router.get('/:id', (req,res) => {
+	res.json({
+		id: this.params.id,
+		name: 'Exact Exact',
+		email: 'exact@gmail.com',
+		city: 'London',
+		tel: '98483247282',
+		status: 'Asylum seeker',
+		country: 'Eritrea',
+		experience: 'very little',
+		itAcess: true,
+		hearAbout: 'website'
+	});
+});
+
+
+
+module.exports=router; 
--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,21 @@ const port = 3001;
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const router = require('./routers/router');
+
+const applicants = require('./routes/applicants');
+const admin = require('./routes/admin');
+
 const app = express();
 
-app.use('/api', router);
+app.use('/api/applicants', applicants);
+app.use('/api/admin', admin);
+
 app.use(bodyParser.json());
 
 app.get('/', (req,res) => {
 	res.status(200).json({status: 200});
 });
-  
+
 app.listen(port, () => {
 	console.log(`Server started on port ${port}`);
 });
