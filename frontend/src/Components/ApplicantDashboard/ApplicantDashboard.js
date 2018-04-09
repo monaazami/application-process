42298258884f1d7ddd1be1f23bd0f3b88594437c
louiaR <louitiani@gmail.com>
2018-04-09 14:27:29 +0100

    fixed some indentations issue

--- frontend/src/Components/ApplicantDashboard/ApplicantDashboard.js
+++ frontend/src/Components/ApplicantDashboard/ApplicantDashboard.js
@@ -3,17 +3,12 @@ import stepsArray from './stepsArray';
 import DashboardStep from './DashboardStep';
 
 const ApplicantDashboard = () => (
-	<section>
-		<h2>Your Progress</h2>
-		{stepsArray.map((step, i) => 
-			<DashboardStep 
-				stepNumber={step.step}
-				details={step.details}
-				key={i}
-			/>
-		)}
-	</section>
+  <section>
+    <h2>Your Progress</h2>
+    {stepsArray.map((step, i) => (
+      <DashboardStep stepNumber={step.step} details={step.details} key={i} />
+    ))}
+  </section>
 );
-
 
 export default ApplicantDashboard;
