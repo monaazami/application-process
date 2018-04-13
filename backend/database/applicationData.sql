78e1f7cebfca718e4676672f95c017f8c717345f
louiaR <louitiani@gmail.com>
2018-04-13 09:49:45 +0100

    Create fake data & edit route/applicant.js

--- backend/database/applicationData.sql
+++ backend/database/applicationData.sql
@@ -12,7 +12,7 @@ create table applicants (
       country         string not null,
       experience      string not null,
       itAcces         string not null,
-      hearAbout     string not null     
+      hearAbout       string not null     
 );
 
 create table steps (
@@ -23,3 +23,4 @@ create table steps (
     url               string default null,
     foreign key (applicant_id) references applicants(id)
 );
+
