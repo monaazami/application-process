090085cf2255657b1fc5c824708151bf3b4ce33a
louiaR <louitiani@gmail.com>
2018-04-13 09:59:22 +0100

    edit applicationData.sql

--- backend/database/applicationData.sql
+++ backend/database/applicationData.sql
@@ -8,7 +8,7 @@ create table applicants (
       email           string not null,
       city            string not null,
       tel             integer not null,
-      status          string not null,
+      status          boolean DEFAULT false,
       country         string not null,
       experience      string not null,
       itAcces         string not null,
