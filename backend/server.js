0330744a447efd7433b06955309b9721561f5383
OlenaKashuba <amely.jazz@gmail.com>
2018-04-18 11:18:03 +0100

    Install cors

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,11 @@ const port = process.env.PORT || 3001;
 const express = require('express');
 const bodyParser = require('body-parser');
 const applicants = require('./routes/applicants');
-
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.use('/api/applicants', applicants);
-
 app.use(bodyParser.json());
 
 app.listen(port, () => {
