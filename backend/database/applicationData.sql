8617c78ccaaa798800e6052918ccd98e400fa33b
Gerald Isabelle <info@geraldisabelle.me>
2018-04-19 20:14:38 +0100

    @louis changes and from end-point-route https://github.com/CodeYourFuture/application-process/pull/18/files

---
+++ backend/database/applicationData.sql
@@ -0,0 +1,23 @@
+PRAGMA foreign_keys = ON;
+
+create table applicants (
+      id              integer primary key,
+      fullName         string not null,
+      email           string not null,
+      city            string not null,
+      tel             integer not null,
+      status          boolean DEFAULT false not null,
+      country         string not null,
+      experience      string not null,
+      itAcces         string not null,
+      hearAbout       string not null
+);
+
+create table steps (
+    id                integer primary key,
+    applicant_id      integer,
+    step_number       integer not null,
+    status            string not null,
+    url               string default null,
+    foreign key (applicant_id) references applicants(id)
+);
--- backend/routes/applicants.js
+++ backend/routes/applicants.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const sqlite = require('sqlite3').verbose();
 const router = require('express').Router();
+
+const fakeApplicants = require('../fakeData/fakeApplicant.json');
 
 const filename = './database/db.sqlite';
 let db = new sqlite.Database(filename, (err) => {
@@ -10,14 +10,47 @@ let db = new sqlite.Database(filename, (err) => {
   console.log('Connected to the in-memory application database.');
 });
 
-//GET ALL APPLICANTS
+
+// get all applicants inside the database
 router.get('/', (req,res) => {
-	let sql = 'select * from applicants';
-	db.all(sql, [], (err, rows) => {
-		res.status(200).json({
-			applicants: rows
-		});
-	});
+  let sql = 'select * applicants';
+  db.all(sql, [], (err, rows) => {
+    res.status(200).json({
+      applicants: rows
+    });
+  });
 });
+
+
+// get one applicant through his id
+router.get('/:id', function (req, res) {
+  let sql = 'select * from applicants where id = ?';
+  db.all(sql, [Number(req.params.id)], (err, rows) => {
+    if (typeof (Number(req.params.id)) !== 'number' || rows.length === 0) {
+      res.send('400 - BAD REQUEST');
+    } else {
+      res.status(200).json({
+        applicants: rows
+      });
+    };
+  })
+});
+
+
+// add new applicant
+router.post("/", (req, res) => {
+  db.run(`INSERT INTO into applicants (fullName, email, tel, status, country, experience, itAcess, hearAbout) Values (?, ?, ?, ?, ?, ?, ?, ?)`, [req.body.fullName, req.body.email, req.body.tel, req.body.status, req.body.country, req.body.experience, req.body.itAccess, req.body.hearAbout], (req, res) =>{
+    if (err) {
+      res.send('400 - BAD REQUEST');
+    }
+    res.status(200).json({
+      where: 'From post request'
+    })
+
+  });
+
+});
+
+
 
 module.exports = router ;
