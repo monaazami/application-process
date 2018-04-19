cc7d7f9fc6903b537ab16bcc0e5f6bfceae063e3
Louis <30389858+LouiaR@users.noreply.github.com>
2018-04-19 15:50:14 +0100

    Revert "End point route"

--- backend/routes/applicants.js
+++ backend/routes/applicants.js
@@ -1,56 +1,59 @@
 const router = require('express').Router();
 
-const fakeApplicants = require('../fakeData/fakeApplicant.json');
-
-const filename = './database/db.sqlite';
-let db = new sqlite.Database(filename, (err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Connected to the in-memory application database.');
-});
-
-
-// get all applicants inside the database
 router.get('/', (req,res) => {
-	let sql = 'select * applicants';
-  db.all(sql, [], (err, rows) => {
-    res.status(200).json({
-      applicants: rows
-    });
-  });
+	res.json([{
+		id: '3432',
+		name: 'John Smith',
+		email: 'johnsmith@gmail.com',
+		city: 'London',
+		tel: '98483247282',
+		status: 'Asylum seeker',
+		country: 'Eritrea',
+		experience: 'very little',
+		itAcess: true,
+		hearAbout: 'website'
+	},
+	{
+		id: '3433',
+		name: 'John Smith',
+		email: 'johnsmith@gmail.com',
+		city: 'London',
+		tel: '98483247282',
+		status: 'Asylum seeker',
+		country: 'Eritrea',
+		experience: 'very little',
+		itAcess: true,
+		hearAbout: 'website'
+	},
+	{
+		id: '3434',
+		name: 'John Smith',
+		email: 'johnsmith@gmail.com',
+		city: 'London',
+		tel: '98483247282',
+		status: 'Asylum seeker',
+		country: 'Eritrea',
+		experience: 'very little',
+		itAcess: true,
+		hearAbout: 'website'
+	}]);
+});
+
+router.get('/:id', (req,res) => {
+	res.json({
+		id: 3432,
+		name: 'Exact Exact',
+		email: 'exact@gmail.com',
+		city: 'London',
+		tel: '98483247282',
+		status: 'Asylum seeker',
+		country: 'Eritrea',
+		experience: 'very little',
+		itAcess: true,
+		hearAbout: 'website'
+	});
 });
 
 
-// get one applicant through his id
-router.get('/:id', function (req, res) {
-  let sql = 'select * from applicants where id = ?';
-  db.all(sql, [Number(req.params.id)], (err, rows) => {
-    if (typeof (Number(req.params.id)) !== 'number' || rows.length === 0) {
-      res.send('400 - BAD REQUEST');
-    } else {
-      res.status(200).json({
-        applicants: rows
-      });
-    };
-  })
-});
 
-
-// add new applicant
-router.post("/", (req, res) => {
-	 db.run(`INSERT INTO into applicants (fullName, email, tel, status, country, experience, itAcess, hearAbout) Values (?, ?, ?, ?, ?, ?, ?, ?)`, [req.body.fullName, req.body.email, req.body.tel, req.body.status, req.body.country, req.body.experience, req.body.itAccess, req.body.hearAbout], (req, res) =>{
-		if (err) {
-			res.send('400 - BAD REQUEST');
-    }
-		res.status(200).json({
-			where: 'From post request'
-	 })
-
-});
-  
-});
-
-
-
-module.exports = router ;
+module.exports=router; 
