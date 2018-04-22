2dfdcd319f319d31a6e46987a4704144f46cdcf2
louiaR <louitiani@gmail.com>
2018-04-22 21:31:24 +0100

    turn applicant dashboard to a class component with stepsarray as initial state

--- frontend/src/Components/ApplicantDashboard/ApplicantDashboard.js
+++ frontend/src/Components/ApplicantDashboard/ApplicantDashboard.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import stepsArray from './stepsArray';
 import DashboardStep from './DashboardStep';
 import './ApplicantDashboard.css';
 
-const ApplicantDashboard = () => (
-  <section className='applicant-dashboard'>
-    <h2>Your Progress</h2>
-    {stepsArray.map((step, i) => (
-      <DashboardStep stepNumber={step.step} details={step.details} key={i} />
-    ))}
-  </section>
-);
+class ApplicantDashboard extends Component {
+  state={
+    steps: stepsArray,
+  }
+ 
+  addUrlHandler = (e) => {
+    e.preventDefault();
+    console.log('from add url')
+    this.setState({
+      [e.target.name]:e.target.value,
+    })
+  }
+  render(){
+    return(
+      <section className='applicant-dashboard'>
+        <h2>Your Progress</h2>
+        {stepsArray.map((step, i) => (
+          <DashboardStep 
+          stepNumber={step.step} 
+          details={step.details}
+          url={this.state.steps[i].url} 
+          addUrl={this.addUrlHandler}
+          key={i} />
+        ))}
+      </section>
+    );
+  }
+}
 
 export default ApplicantDashboard;
