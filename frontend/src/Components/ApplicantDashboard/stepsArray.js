b2bb0122f659f67a259a5bbbdc717837b801c201
louiaR <louitiani@gmail.com>
2018-04-22 21:18:57 +0100

    Add url key to stepaArrays

--- frontend/src/Components/ApplicantDashboard/stepsArray.js
+++ frontend/src/Components/ApplicantDashboard/stepsArray.js
@@ -1,8 +1,8 @@
 export default [
-	{step: 0, details: 'Website Application'},
-	{step: 1, details: 'Khan Academy: Basics Tutorial /  Solo Learn: Basics Tutorial'},
-	{step: 2, details: 'Code Academy: HTML & CSS Tutorial'},
-	{step: 3, details: 'Code Academy: Responsive Design Tutorial'},
-	{step: 4, details: 'Create a Web Page'},
-	{step: 5, details: 'Iterate a Web Page'}
+	{step: 0, details: 'Website Application', url: ''},
+	{step: 1, details: 'Khan Academy: Basics Tutorial /  Solo Learn: Basics Tutorial', url: ''},
+	{step: 2, details: 'Code Academy: HTML & CSS Tutorial', url: ''},
+	{step: 3, details: 'Code Academy: Responsive Design Tutorial', url: ''},
+	{step: 4, details: 'Create a Web Page', url: ''},
+	{step: 5, details: 'Iterate a Web Page', url: ''}
 ]
