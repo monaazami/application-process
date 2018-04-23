a48866dff207054fa252f4743a88585c50ba5a6c
louiaR <louitiani@gmail.com>
2018-04-23 15:00:56 +0100

    Delete stepsArray.js

--- frontend/src/Components/ApplicantDashboard/stepsArray.js
+++
@@ -1,8 +0,0 @@
-export default [
-	{step: 0, details: 'Website Application', url: ''},
-	{step: 1, details: 'Khan Academy: Basics Tutorial /  Solo Learn: Basics Tutorial', url: ''},
-	{step: 2, details: 'Code Academy: HTML & CSS Tutorial', url: ''},
-	{step: 3, details: 'Code Academy: Responsive Design Tutorial', url: ''},
-	{step: 4, details: 'Create a Web Page', url: ''},
-	{step: 5, details: 'Iterate a Web Page', url: ''}
-]
