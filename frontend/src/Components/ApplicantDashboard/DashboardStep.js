f92a72888b319244374203cce117d86556e696ad
louiaR <louitiani@gmail.com>
2018-04-23 10:14:25 +0100

    Make submitBlock a form instead of a div

--- frontend/src/Components/ApplicantDashboard/DashboardStep.js
+++ frontend/src/Components/ApplicantDashboard/DashboardStep.js
@@ -1,28 +1,34 @@
 import React from 'react';
 import StatusMessage from './StatusMessage';
 
-const DashboardStep = ({ stepNumber, details, url, addUrl}) => {
-  console.log(url)
+const DashboardStep = ({ stepNumber, details, url, addUrl }) => {
   let submitBlock;
   if (stepNumber !== 0) {
     submitBlock = (
-      <div>
-        <input 
-          type='text'
-          placeholder='Add url here' 
-          required
-          name="url"
-          value={url}
-          onChange={addUrl}
+      <form>
+        <div className="form-group">
+          <input
+            required          
+            type="text"
+            placeholder="Add url here"
+            name="url"
+            value={url}
+            onChange={addUrl}
           />
-        <button className='btn btn-secondary' type='submit'>Submit step</button>
-      </div>
+          <button className="btn btn-secondary" type="submit">
+            Submit step
+          </button>
+          <small id="emailHelp" class="form-text text-muted">Please make sure the link you have provided is revelant to this step and valid.</small>
+        </div>
+      </form>
     );
   }
   return (
-    <section className='dashboard-step'>
+    <section className="dashboard-step">
       <h3>Step {stepNumber}</h3>
-      <p><b>{details}</b></p>
+      <p>
+        <b>{details}</b>
+      </p>
       {submitBlock}
       <StatusMessage status={stepNumber !== 0 ? '' : 'Approved'} />
     </section>
