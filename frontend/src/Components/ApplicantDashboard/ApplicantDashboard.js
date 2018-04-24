9487792ff2bcf43ca86b5d90911ca266ca8fa9df
louiaR <louitiani@gmail.com>
2018-04-24 15:52:34 +0100

    Add comments

--- frontend/src/Components/ApplicantDashboard/ApplicantDashboard.js
+++ frontend/src/Components/ApplicantDashboard/ApplicantDashboard.js
@@ -10,7 +10,8 @@ class ApplicantDashboard extends Component {
     steps: helpers.stepsArray,
   }
  
-  addUrlHandler = (e, id) => {  
+  addUrlHandler = (e, id) => { 
+    // target and only change the input field we want to write in 
     const stepIndex = this.state.steps.findIndex(myStep => {
       return myStep.step === id;
     })
@@ -21,6 +22,8 @@ class ApplicantDashboard extends Component {
     const steps = [...this.state.steps];
     steps[stepIndex].url = e.target.value;
     this.setState({ steps: steps });
+
+    //check url is valid and that link is a substract of that url display a message when none of these condition are meeted
     if (helpers.ValidURL(step.url) && (helpers.containPartOf(step.url, step.link) !== -1)) {
       step.alert = '';      
       const steps = [...this.state.steps];
@@ -50,8 +53,8 @@ class ApplicantDashboard extends Component {
       ...this.state.steps[stepIndex]
     };
     
+    // send data backend only if url submitted meet the requirement
     if (helpers.ValidURL(step.url) && (helpers.containPartOf(step.url, step.link) !== -1)) {
-
       const steps = [...this.state.steps];
       steps[stepIndex] = step;
       step.status = true;
@@ -71,7 +74,8 @@ class ApplicantDashboard extends Component {
       .catch(error => {
         console.log(error.message);
       });
-
+      
+      // for all these cases don't post submit the url
     } else if (helpers.ValidURL(step.url) && (helpers.containPartOf(step.url, step.link) === -1)){
       step.alert = 'Please make sure the link is valid and relevant to this step. ';
       const steps = [...this.state.steps];
