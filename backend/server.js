eb53352bced5f5d20ae79f22bc11545dec901b36
OlenaKashuba <amely.jazz@gmail.com>
2018-04-24 15:33:48 +0100

    Add backend changes

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const cors = require('cors');
 const applicants = require('./routes/applicants');
+const dashboard = require('./routes/dashboard');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));// support encoded bodies
@@ -12,6 +13,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use('/api/applicants', applicants);
+app.use('/api/dashboard', dashboard);
 app.listen(port, () => {
 	console.log(`Server started on port ${port}`);
 });
