1a2a9695a8afe9fb21ee84de31330c97a29e9dcb
OlenaKashuba <amely.jazz@gmail.com>
2018-04-27 13:17:28 +0100

    Adjust formsubmittedmessage text

--- frontend/src/Components/ApplicationForm/FormSubmittedMessage.js
+++ frontend/src/Components/ApplicationForm/FormSubmittedMessage.js
@@ -8,21 +8,22 @@ const FormSubmittedMessage = ({ submitted, hideThankyouMessage, fullName, email,
       'block': (submitted === true),
       'hidden': (submitted === false)
     })}>
-      <button type="button" className="close" aria-label="Close" onClick={hideThankyouMessage}>
-        <span aria-hidden="true">&times;</span>
+      <button type='button' className='close' aria-label='Close' onClick={hideThankyouMessage}>
+        <span aria-hidden='true'>&times;</span>
       </button>
       <img
-        src="https://upload.wikimedia.org/wikipedia/commons/7/73/Flat_tick_icon.svg"
-        alt="green tick"
-        height="150"
-        width="150"
-        className="img-fluid mt-5 pt-4"
+        src='https://upload.wikimedia.org/wikipedia/commons/7/73/Flat_tick_icon.svg'
+        alt='green tick'
+        height='150'
+        width='150'
+        className='img-fluid mt-5 pt-4'
       />
-      <h1 className="mt-5 pb-3 display-5">Thank you, {fullName}! </h1>
-      <h3 className="pt-4 pb-3 display-5">
-        Your detail have been submitted sucessfully. We have send an  email on {email}. Please check your email.
-      </h3>
-      <p> Here is a link to your dashboard <Link to={`/applicant-dashboard/${id}`} onClick={hideThankyouMessage}> Link yo your personal dashboard </Link></p>
+      <h2 className='mt-5 pb-3 display-5'>Thank you, {fullName}! </h2>
+      <p className='pt-4 pb-3 display-5 thanks-message'>
+        Your detail have been submitted sucessfully.
+      </p>
+      <p className='link-message'> Here is a link to your dashboard: <Link to={`/applicant-dashboard/${id}`} onClick={hideThankyouMessage}> Click here! </Link></p>
+      <p className='important-message'> <b>Important!</b> Copy and save this link to access your dashboard in the future </p>
     </div>
   );
 };
